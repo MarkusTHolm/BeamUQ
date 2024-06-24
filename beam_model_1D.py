ba95dcd51693a0eade27b7a55c7ad6239c36daf5
MarkusTHolm <mtaho@dtu.dk>
2024-06-24 09:12:52 +0200

    Cleaned notebook and fixed implementation of variables

--- beam_model_1D.py
+++ beam_model_1D.py
@@ -18,37 +18,34 @@ sys.path.append(module_path)
 
 import cppFem
 
-# Settings
-E = 210e9
-nu = 0.3
-width = 0.5
-
-Fx = 3.0e9
-Fy = 1.5e7 
-
 class BeamModel1D:
     def __init__(self, nelx, L):
+        # Parameters
         self.L = L
+        self.E = 210e9
+        self.nu = 0.3
+        self.width = 0.5
+        self.Fx = 3.0e9
+        self.Fy = 0.5e7 
 
         self.xMat, self.ixMat = self.create_mesh(nelx)
 
-        self.materialMat = np.array([[E, nu, width],
-                                     [E, nu, width]])
+        self.materialMat = np.array([[self.E, self.nu, self.width],
+                                     [self.E, self.nu, self.width]])
         
         # Assemble load vector
         nel = np.shape(self.ixMat)[0]
         nn = np.shape(self.xMat)[0]
         ndof = nn*3
-
-        self.fVec = np.zeros((ndof, 1))
+        self.fVec = np.zeros((ndof, ))
         # self.fVec[-3] = Fx
-        self.fVec[-2] = Fy
+        self.fVec[-2] = self.Fy
        
-    def forward(self, thkVec):
+    def forward(self, thk):
 
         # Assemble stifness matrix
         kMat = cppFem.assemble_tangent_stiffness(self.xMat, self.ixMat, 
-                                                 self.materialMat, thkVec)
+                                                 self.materialMat, thk)
 
         # Apply Dirichlet BC's
         dofs = [0, 1, 2] 
@@ -58,22 +55,24 @@ class BeamModel1D:
 
         # Solve system
         # kMat = 0.5 * (kMat.T + kMat)     # Enforce symmetry
-        kMat = kMat.tocoo()
-        K = cvxopt.spmatrix(kMat.data, 
-                            kMat.row.astype(np.int64), 
-                            kMat.col.astype(np.int64))
-        B = cvxopt.matrix(self.fVec)
-        cvxopt.cholmod.linsolve(K, B)
-        u = np.array(B)[:, 0]
+        # kMat = kMat.tocoo()
+        # K = cvxopt.spmatrix(kMat.data, 
+        #                     kMat.row.astype(np.int64), 
+        #                     kMat.col.astype(np.int64))
+        # B = cvxopt.matrix(self.fVec)
+        # cvxopt.cholmod.linsolve(K, B)
+        # u = np.array(B)[:, 0]
+        kMat = kMat.tocsc()
+        u = sp.linalg.spsolve(kMat, self.fVec)
+        v = u[1::3]
 
-        return u[1::3]
+        return v
 
     def create_mesh(self, nelx):
         """ Create a line mesh (see example below)"""        
         """
             nelx(=5), nx(=6), L
         o-----o-----o-----o-----o-----o
-
         """
 
         # Initialize
@@ -110,12 +109,8 @@ class BeamModel1D:
 
         return xMat, ixMat
 
-thkVec = np.array([1.0, 1.0, 1.0])
-
-nelx = 3
-L = 1.5
-model = BeamModel1D(nelx, L)
-
-u = model.forward(thkVec)
-
-print(u)
+if __name__ == '__main__':
+    nelx = 3
+    beam = BeamModel1D(nelx=nelx, L=1.5)
+    v = beam.forward(thkVec=np.ones(nelx))
+    print(f"v = {v}")
